3331c30924d25fdb46a8de50574a13cedf7f730b
minea <minea@jin>
2023-03-19 22:00:47 +0900

    컴파일 에러 ?

--- Java200/src/sec03/Ex17.java
+++ Java200/src/sec03/Ex17.java
@@ -16,6 +16,8 @@ public class Ex17 {
 		if (a == b) {
 			System.out.println("두 수는 같습니다.");
 		} else {
+			int max, min;
+
 			if (a > b) {
 				int max = a;
 				int min = b;
@@ -23,10 +25,11 @@ public class Ex17 {
 				int max = b;
 				int min = a;
 			}
+			System.out.println("작은 값은 " + min + "입니다.");
+			System.out.println("큰 값은 " + max + "입니다.");
 		}
 		
-		System.out.println("작은 값은 " + min + "입니다.");
-		System.out.println("큰 값은 " + max + "입니다.");
+
 		
 	}
 
